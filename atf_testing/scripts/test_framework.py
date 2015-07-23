b1f0d0518654f71dd716f9bff8b0ca3fb69c32b5
Florian Köhler <Florian.Koehler@gmx.net>
2015-07-23 11:55:28 +0200

    testblocks and metrics are read from test_config file

--- atf_testing/scripts/test_framework.py
+++ atf_testing/scripts/test_framework.py
@@ -1,14 +1,67 @@
 #!/usr/bin/env python
 import rospy
+import rospkg
+import yaml
+import rosparam
 
 from atf_testing import ATF, Testblock
-from atf_metrics import CalculatePathLength, CalculateTime
+from atf_metrics import CalculatePathLength, CalculateTime, CalculateResources
+
+
+def create_test_list():
+
+    test_config_path = rospkg.RosPack().get_path("cob_benchmarking") + "/config/test_config.yaml"
+    config_data = load_data(test_config_path)[rosparam.get_param("/test_name")]
+
+    test_list_int = []
+
+    for testblock in config_data:
+        metrics = []
+        # Time
+        try:
+            config_data[testblock]["time"]
+        except KeyError:
+            pass
+        else:
+            metrics.append(CalculateTime())
+
+        # Path length
+        try:
+            config_data[testblock]["path_length"]
+        except KeyError:
+            pass
+        else:
+            for item in config_data[testblock]["path_length"]:
+                metrics.append(CalculatePathLength(item[0], item[1]))
+
+        # Resources
+        try:
+            config_data[testblock]["resources"]
+        except KeyError:
+            pass
+        else:
+            resource = {}
+            for item in config_data[testblock]["resources"]:
+                resource.update({item: config_data[testblock]["resources"][item]})
+            metrics.append(CalculateResources(resource))
+
+        test_list_int.append(Testblock(testblock, metrics))
+
+    return test_list_int
+
+
+def load_data(filename):
+
+        with open(filename, 'r') as stream:
+            doc = yaml.load(stream)
+        return doc
 
 
 if __name__ == '__main__':
-    rospy.init_node("atf")
+    rospy.init_node("test_framework")
     
     # Define metrics
+    '''
     L1 = CalculatePathLength("base_link", "gripper_right_grasp_link")
     L2 = CalculatePathLength("base_link", "arm_right_3_link")
     L3 = CalculatePathLength("base_link", "gripper_right_grasp_link")
@@ -21,8 +74,13 @@ if __name__ == '__main__':
     T6 = CalculateTime()
     T7 = CalculateTime()
     T8 = CalculateTime()
-    
-    test_list = [Testblock("execution_2", [L1, L2, T1]),
+    R1 = CalculateResources({"cpu":["move_group"], "mem":["move_group"], "io":["move_group"], "network":["move_group"]})
+    '''
+
+    test_list = create_test_list()
+
+    '''
+    test_list = [Testblock("execution_2", [L1, L2, T1, R1]),
                  Testblock("planning_all", [T2]),
                  Testblock("planning_1", [T3]),
                  Testblock("planning_2", [T4]),
@@ -30,5 +88,6 @@ if __name__ == '__main__':
                  Testblock("execution_all", [T6, L3, L4]),
                  Testblock("execution_1", [T7]),
                  Testblock("execution_3", [T8])]
+    '''
 
     ATF(test_list).wait_for_end()
