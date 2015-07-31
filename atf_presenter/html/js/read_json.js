00102ac76f928bf0ac1caecb81685c04afa46c2c
Florian Köhler <Florian.Koehler@gmx.net>
2015-07-31 16:37:19 +0200

    switched to bootstrap framework (first steps...)

--- atf_presenter/html/js/read_json.js
+++ atf_presenter/html/js/read_json.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+    $(".content_container").load("pages/home.html");
+});
+
+$(".nav-sidebar li").each(function() {
+    $(this).on("click", function() {
+        $(".content_container").load($(this).attr("data-page"))
+    });
+});
+
+$(".nav-sidebar li").each(function() {
+    $(this).on("click", function() {
+        $(".content_container").load($(this).attr("data-page"))
+    });
+});
+
+$('.sidebar > ul.nav li a').click(function(e) {
+    $('.sr-only').remove();
+    var $this = $(this);
+    $this.parent().siblings().removeClass('active').end().addClass('active');
+    $this.append('<span class="sr-only">(current)</span>');
+    e.preventDefault();
+});
+
 function getJSON() {
     $.getJSON("js/ts1_t1.json")
         .done(function( json ) {
