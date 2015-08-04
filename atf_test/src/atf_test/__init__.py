659d808eaa73a2e61c84d682936f4ce6b2d793a3
Florian Köhler <Florian.Koehler@gmx.net>
2015-08-04 15:18:48 +0200

    added working automated example for recording and analysing

---
+++ atf_test/src/atf_test/__init__.py
@@ -0,0 +1,1 @@
+from publish_tf import PublishTf
--- atf_test/src/atf_test/publish_tf.py
+++ atf_test/src/atf_test/publish_tf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import sys
 import math
 import rospy
@@ -23,6 +22,23 @@ class PublishTf:
         rospy.Timer(rospy.Duration(1/self.pub_freq), self.reference4)
         rospy.sleep(1)
 
+        recorder_0 = RecordingManager("all")
+        recorder_1 = RecordingManager("test1")
+        recorder_2 = RecordingManager("test2")
+        recorder_3 = RecordingManager("test3")
+
+        recorder_0.start()
+        recorder_1.start()
+        self.pub_line(length=-1, time=2.5)
+        recorder_1.stop()
+        recorder_2.start()
+        self.pub_quadrat(length=2, time=10)
+        recorder_2.stop()
+        recorder_3.start()
+        self.pub_circ(radius=2, time=10)
+        recorder_3.stop()
+        recorder_0.stop()
+
     def reference2(self, event):
         self.check_for_ctrlc()
         self.pub_tf(self.child1_frame_id, self.child2_frame_id, [1, 0, 0])
@@ -42,48 +58,48 @@ class PublishTf:
             rpy[0], rpy[1], rpy[2]), rospy.Time.now(), child1_frame_id, parent_frame_id)
 
     def pub_line(self, length=1, time=1):
-        print "line"
+        rospy.loginfo("Line")
         rate = rospy.Rate(self.pub_freq)
 
         for i in range((int(self.pub_freq*time/2)+1)):
             t = i/self.pub_freq/time*2
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [t*length, 0, 0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t*length, 0, 0])
             rate.sleep()
         for i in range((int(self.pub_freq*time/2)+1)):
             t = i/self.pub_freq/time*2
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1-t)*length, 0, 0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1-t)*length, 0, 0])
             rate.sleep()
-                     
+
     def pub_circ(self, radius=1, time=1):
-        print "circ"
+        rospy.loginfo("Circ")
         rate = rospy.Rate(self.pub_freq)
 
         for i in range(int(self.pub_freq*time)+1):
             t = i/self.pub_freq/time
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [-radius*math.cos(2*math.pi*t)+radius,
-                                                                    -radius*math.sin(2*math.pi*t),
-                                                                    0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [-radius*math.cos(2*math.pi*t)+radius,
+                                                                     -radius*math.sin(2*math.pi*t),
+                                                                     0])
             rate.sleep()
 
     def pub_quadrat(self, length=1, time=1):
-        print "quadrat"
+        rospy.loginfo("Quadrat")
         rate = rospy.Rate(self.pub_freq)
 
         for i in range((int(self.pub_freq*time/4)+1)):
             t = i/self.pub_freq/time*4
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [t*length, 0, 0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t*length, 0, 0])
             rate.sleep()
         for i in range((int(self.pub_freq*time/4)+1)):
             t = i/self.pub_freq/time*4
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [length, t*length, 0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [length, t*length, 0])
             rate.sleep()
         for i in range((int(self.pub_freq*time/4)+1)):
             t = i/self.pub_freq/time*4
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1-t)*length, length, 0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1-t)*length, length, 0])
             rate.sleep()
         for i in range((int(self.pub_freq*time/4)+1)):
             t = i/self.pub_freq/time*4
-            PTf.pub_tf(self.parent_frame_id, self.child1_frame_id, [0, (1-t)*length, 0])
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [0, (1-t)*length, 0])
             rate.sleep()
 
     @staticmethod
@@ -91,25 +107,8 @@ class PublishTf:
         if rospy.is_shutdown():
             sys.exit()
 
-if __name__ == '__main__':
-    rospy.init_node('atf_test')
-    rospy.loginfo("Running...")
-
-    PTf = PublishTf()
-
-    recorder_1 = RecordingManager("test1")
-    recorder_2 = RecordingManager("test2")
-    recorder_3 = RecordingManager("test3")
-
-    recorder_1.start()
-    PTf.pub_line(length=-1, time=2.5)
-    recorder_1.stop()
-    recorder_2.start()
-    PTf.pub_quadrat(length=2, time=5)
-    recorder_2.stop()
-    recorder_3.start()
-    PTf.pub_circ(radius=2, time=5)
-    recorder_3.stop()
-
-    rospy.loginfo("Closing...")
-    sys.exit()
+'''
+if __name__ == "__main__":
+    rospy.init_node("publish_tf")
+    PublishTf().start
+'''
