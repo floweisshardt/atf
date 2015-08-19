f759a102163a98e3dcc67d20584708032080ff61
Florian Köhler <Florian.Koehler@gmx.net>
2015-08-19 16:23:02 +0200

    added package for recorder plugins

--- atf_recorder_plugins/src/atf_recorder_plugins/plugin_resources.py
+++ atf_recorder_plugins/src/atf_recorder_plugins/plugin_resources.py
@@ -6,98 +6,87 @@ import time
 import xmlrpclib
 import rosnode
 
+from copy import copy
 from subprocess import check_output, CalledProcessError
 from atf_msgs.msg import *
+from atf_recorder import BagfileWriter
 
 
 class RecordResources:
-    def __init__(self, topic, config_file, lock, bag_file):
-        self.topic = topic
+    def __init__(self, topic_prefix, config_file, write_lock, bag_file):
+        self.topic_prefix = topic_prefix
         self.test_config = config_file
-        self.lock = lock
-        self.bag_file = bag_file
 
         self.resources_timer_frequency = 4.0  # Hz
         self.timer_interval = 1/self.resources_timer_frequency
 
         self.testblock_list = self.create_testblock_list()
+        self.pid_list = self.create_pid_list()
         self.requested_nodes = {}
-        self.res_pipeline = []
+        self.res_pipeline = {}
+
+        self.BW = BagfileWriter(bag_file, write_lock)
 
         rospy.Timer(rospy.Duration.from_sec(self.timer_interval), self.collect_resource_data)
 
     def update_requested_nodes(self, name, command):
-        update_pipeline = False
 
-        try:
-            self.test_config[name]["path_length"]
-        except KeyError:
-            pass
-        else:
-            if command == "add":
-                self.tf_active = True
-                self.requested_nodes["path_length"].append(True)
-            elif command == "del":
-                self.tf_active = False
-                self.requested_nodes["path_length"].remove(True)
-            update_pipeline = True
+        if command == "add":
+            for node in self.testblock_list[name]:
+                if node not in self.requested_nodes:
+                    self.requested_nodes[node] = copy(self.testblock_list[name][node])
+                    self.res_pipeline[node] = copy(self.testblock_list[name][node])
+                else:
+                    for res in self.testblock_list[name][node]:
+                        self.requested_nodes[node].append(res)
+                        if res not in self.res_pipeline[node]:
+                            self.res_pipeline[node].append(res)
 
-        try:
-            resources_temp = self.test_config[name]["resources"]
-        except KeyError:
-            pass
-        else:
-            for res in resources_temp:
-                for item in resources_temp[res]:
-                    if command == "add":
-                        self.requested_nodes[res].append(item)
-                    elif command == "del":
-                        self.requested_nodes[res].remove(item)
-            update_pipeline = True
-
-        if update_pipeline:
-            self.res_pipeline = {}
-
-            for item in self.requested_nodes:
-                for value in self.requested_nodes[item]:
-                    if not type(value) == bool:
-                        if value not in self.res_pipeline:
-                            self.res_pipeline[value] = [item]
-                        elif item not in self.res_pipeline[value]:
-                            self.res_pipeline[value].append(item)
-                    else:
-                        self.tf_active = value
+        elif command == "del":
+            for node in self.testblock_list[name]:
+                for res in self.testblock_list[name][node]:
+                    self.requested_nodes[node].remove(res)
+                    if res not in self.requested_nodes[node]:
+                        self.res_pipeline[node].remove(res)
+                    if len(self.requested_nodes[node]) == 0:
+                        del self.requested_nodes[node]
+                        del self.res_pipeline[node]
 
     def create_testblock_list(self):
+
         testblock_list = {}
         for testblock in self.test_config:
-            for resource in self.test_config[testblock]["resources"]:
-                try:
-                    testblock_list[testblock]
-                except KeyError:
-                    testblock_list.update({testblock: {}})
+            try:
+                self.test_config[testblock]["resources"]
+            except KeyError:
+                continue
+            else:
+                for resource in self.test_config[testblock]["resources"]:
+                    try:
+                        testblock_list[testblock]
+                    except KeyError:
+                        testblock_list.update({testblock: {}})
 
-                for node_name in self.test_config[testblock]["resources"][resource]:
+                    for node_name in self.test_config[testblock]["resources"][resource]:
 
-                    if node_name not in testblock_list[testblock]:
-                        testblock_list[testblock].update({node_name: {"pid": self.get_pid(node_name),
-                                                                      "resources": [resource]}})
-                    elif resource not in testblock_list[testblock][node_name]["resources"]:
-                        testblock_list[testblock][node_name]["resources"].append(resource)
+                        if node_name not in testblock_list[testblock]:
+                            testblock_list[testblock].update({node_name: [resource]})
+                        elif resource not in testblock_list[testblock][node_name]:
+                            testblock_list[testblock][node_name].append(resource)
 
         return testblock_list
 
     def collect_resource_data(self, event):
-        pipeline = self.res_pipeline
+        pipeline = copy(self.res_pipeline)
         if not len(pipeline) == 0:
             msg = Resources()
-            topic = self.topic + "Resources"
+            topic = self.topic_prefix + "Resources"
 
             for node in pipeline:
                 msg_data = NodeResources()
-                try:
-                    pid = self.nodes[node]
-                except KeyError:
+                pid = self.pid_list[node]
+
+                if pid is None:
                     continue
 
                 try:
@@ -131,16 +120,25 @@ class RecordResources:
                 except (psutil.NoSuchProcess, psutil.AccessDenied):
                     pass
 
-            self.write_to_bagfile(topic, msg, rospy.Time.from_sec(time.time()))
+            self.BW.write_to_bagfile(topic, msg, rospy.Time.from_sec(time.time()))
 
     def trigger_callback(self, msg):
+        if msg.name in self.testblock_list:
+            if msg.trigger.trigger == Trigger.ACTIVATE:
+                self.update_requested_nodes(msg.name, "add")
+            elif msg.trigger.trigger == Trigger.FINISH:
+                self.update_requested_nodes(msg.name, "del")
+            elif msg.trigger.trigger == Trigger.ERROR:
+                self.res_pipeline = []
 
-        if msg.trigger.trigger == Trigger.ACTIVATE:
-            self.update_requested_nodes(msg.name, "add")
-        elif msg.trigger.trigger == Trigger.FINISH:
-            self.update_requested_nodes(msg.name, "del")
-        elif msg.trigger.trigger == Trigger.ERROR:
-            self.res_pipeline = []
+    def create_pid_list(self):
+        node_list = {}
+        for (testblock, nodes) in self.testblock_list.iteritems():
+            for node in nodes:
+                if node not in node_list:
+                    node_list[node] = self.get_pid(node)
+
+        return node_list
 
     @staticmethod
     def get_pid(name):
@@ -166,8 +164,3 @@ class RecordResources:
 
         rospy.logerr("Node '" + name + "' is not running or does not exist!")
         return None
-
-    def write_to_bagfile(self, topic, data, set_time):
-        self.lock.acquire()
-        self.bag_file.write(topic, data, set_time)
-        self.lock.release()
