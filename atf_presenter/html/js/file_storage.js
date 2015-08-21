9bbee2dc99dde36f3ad0f80d172f5c5765df5509
Florian Köhler <Florian.Koehler@gmx.net>
2015-08-21 13:29:34 +0200

    added js file for file storage operations

---
+++ atf_presenter/html/js/file_storage.js
@@ -0,0 +1,35 @@
+function writeDataToStorage(name, data) {
+    if (!name || !data) {
+        return;
+    }
+
+    if (typeof data === "object") {
+        data = JSON.stringify(data);
+    }
+    sessionStorage.setItem(name, data);
+}
+
+function getDataFromStorage(name) {
+    var data = sessionStorage.getItem(name);
+
+    if (!data) {
+        return;
+    }
+
+    // assume it is an object that has been stringified
+    if (data[0] === "[" && data[1] === "{") {
+        data = JSON.parse(data);
+    }
+
+    return data;
+}
+
+$(document).on('change', '.btn-file :file', function() {
+    var labels = [];
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1;
+    for (var files = 0; files < input.get(0).files.length; files++) {
+        labels.push(input.get(0).files[parseInt(files)].name.split(".")[0]);
+    }
+    input.trigger('fileselect', [numFiles, labels]);
+});
