3d999f0177ade56808fc2de6bd2c5fb2efd8c0a8
Florian Köhler <Florian.Koehler@gmx.net>
2015-09-11 14:24:10 +0200

    distance checking is working in real-time now

--- obstacle_distance/include/obstacle_distance/obstacle_distance.h
+++ obstacle_distance/include/obstacle_distance/obstacle_distance.h
@@ -1,17 +1,37 @@
 #ifndef OBSTACLE_DISTANCE_OBSTACLE_DISTANCE_H
 #define OBSTACLE_DISTANCE_OBSTACLE_DISTANCE_H
 
-class ObstacleDistance
+#include <ros/ros.h>
+
+// MoveIt!
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit_msgs/GetStateValidity.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/GetPlanningScene.h>
+#include <moveit/collision_detection_fcl/collision_world_fcl.h>
+
+#include <boost/thread.hpp>
+#include <sensor_msgs/JointState.h>
+
+class ObstacleDistance : public ros::NodeHandle
 {
 public:
     ObstacleDistance();
     ~ObstacleDistance();
 
-    void getDistanceToObstacles(std::vector<std::string> link_names);
-
 private:
     planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_;
     std::string PLANNING_SCENE_SERVICE;
+
+    ros::Subscriber tf_subscriber;
+    ros::Timer distance_timer;
+    std::vector<std::string> requested_links;
+    sensor_msgs::JointState current_joint_states;
+
+    void joint_state_callback(const sensor_msgs::JointStatePtr &joint_states);
+    void getDistanceToObstacles(const ros::TimerEvent&);
+    void getPlanningScene();
+
 };
 
 #endif //OBSTACLE_DISTANCE_OBSTACLE_DISTANCE_H
--- obstacle_distance/src/obstacle_distance.cpp
+++ obstacle_distance/src/obstacle_distance.cpp
@@ -1,18 +1,9 @@
-#include <ros/ros.h>
-
-// MoveIt!
-#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
-#include <moveit_msgs/GetStateValidity.h>
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/GetPlanningScene.h>
 #include <obstacle_distance/obstacle_distance.h>
-#include <moveit/collision_detection_fcl/collision_world_fcl.h>
 
-//Class inherited from collision world fcl
-class MyCollisionWorld: public collision_detection::CollisionWorldFCL
+class CreateCollisionWorld: public collision_detection::CollisionWorldFCL
 {
 public:
-    MyCollisionWorld(const collision_detection::WorldPtr & world) :
+    CreateCollisionWorld(const collision_detection::WorldPtr & world) :
             CollisionWorldFCL(world)
     {
     }
@@ -28,11 +19,10 @@ public:
     }
 };
 
-//Class inherited from collision robot fcl
-class MyCollisionRobot: public collision_detection::CollisionRobotFCL
+class CreateCollisionRobot: public collision_detection::CollisionRobotFCL
 {
 public:
-    MyCollisionRobot(const robot_model::RobotModelConstPtr &model) :
+    CreateCollisionRobot(const robot_model::RobotModelConstPtr &model) :
             CollisionRobotFCL(model)
     {
     }
@@ -45,63 +35,86 @@ public:
     }
 };
 
-void ObstacleDistance::getDistanceToObstacles(std::vector<std::string> link_names)
+void ObstacleDistance::getDistanceToObstacles(const ros::TimerEvent&)
 {
-    planning_scene_monitor_->requestPlanningSceneState(PLANNING_SCENE_SERVICE);
-    ROS_INFO("Service requested");
-    planning_scene_monitor::LockedPlanningSceneRW ps(planning_scene_monitor_);
-    ps->getCurrentStateNonConst().update();
+    planning_scene_monitor::LockedPlanningSceneRO ps(planning_scene_monitor_);
 
     planning_scene::PlanningScenePtr planning_scene_ptr = ps->diff();
 
-    MyCollisionWorld cworld(planning_scene_ptr->getWorldNonConst());
-    robot_state::RobotState state(planning_scene_ptr->getCurrentState());
-    // setJointPositions(const std::string &joint_name, const double *position)
-    state.update();
-    state.updateCollisionBodyTransforms();
+    CreateCollisionWorld collision_world(planning_scene_ptr->getWorldNonConst());
+    robot_state::RobotState robot_state(planning_scene_ptr->getCurrentState());
 
-    MyCollisionRobot crobot(state.getRobotModel());
+    for (int i = 0; i < current_joint_states.name.size(); i++)
+    {
+        robot_state.setVariablePosition(current_joint_states.name[i], current_joint_states.position[i]);
+    }
+
+    robot_state.update();
+    robot_state.updateCollisionBodyTransforms();
+
+    CreateCollisionRobot collision_robot(robot_state.getRobotModel());
     std::vector<boost::shared_ptr<fcl::CollisionObject> > robot_obj, world_obj;
 
-    crobot.getCollisionObject(state, robot_obj);
-    cworld.getCollisionObject(world_obj);
+    collision_robot.getCollisionObject(robot_state, robot_obj);
+    collision_world.getCollisionObject(world_obj);
 
     fcl::DistanceRequest req;
     fcl::DistanceResult res;
-    double d;
 
     for (int i = 0; i < robot_obj.size(); i++)
     {
         const collision_detection::CollisionGeometryData* cd = static_cast<const collision_detection::CollisionGeometryData*>(robot_obj[i]->collisionGeometry()->getUserData());
 
-        for (int it = 0; it < link_names.size(); it++)
+        for (int it = 0; it < requested_links.size(); it++)
         {
-            if (link_names[it].compare(cd->getID()) == 0)
+            if (requested_links[it].compare(cd->getID()) == 0)
             {
+                double min_distance = 999;
                 for (int j = 0; j < world_obj.size(); j++)
                 {
                     double dist = fcl::distance(robot_obj[i].get(), world_obj[j].get(), req, res);
-
-                    //std::cout << "Robot Link " << cd->getID() << " at (" << robot_obj[i]->getTranslation().data.vs[0] << ", " << robot_obj[i]->getTranslation().data.vs[1] << ", " << robot_obj[i]->getTranslation().data.vs[2] << ")" << std::endl;
-                    //std::cout << "Object at (" << world_obj[j]->getTranslation().data.vs[0] << ", " << world_obj[j]->getTranslation().data.vs[1] << ", " << world_obj[j]->getTranslation().data.vs[2] << ")" << std::endl;
-                    std::cout << cd->getID() << ": Minimal distance " << dist << std::endl;
+                    if (dist < min_distance)
+                    {
+                        min_distance = fcl::distance(robot_obj[i].get(), world_obj[j].get(), req, res);
+                    }
                 }
+                ROS_INFO("Minimal distance for %s: %f", cd->getID().c_str(), min_distance);
             }
         }
     }
-    ROS_INFO("Calculation done");
+}
+
+void ObstacleDistance::joint_state_callback(const sensor_msgs::JointStatePtr &joint_states)
+{
+    current_joint_states = *joint_states;
+}
+
+void ObstacleDistance::getPlanningScene()
+{
+    while(ros::ok())
+    {
+        planning_scene_monitor_->requestPlanningSceneState(PLANNING_SCENE_SERVICE);
+    }
 }
 
 ObstacleDistance::ObstacleDistance()
+        : ros::NodeHandle()
 {
     PLANNING_SCENE_SERVICE = "get_planning_scene";
+    requested_links.push_back("gripper_right_grasp_link");
+    requested_links.push_back("gripper_left_grasp_link");
 
     //Initialize planning scene monitor
     boost::shared_ptr<tf::TransformListener> tf_listener_(new tf::TransformListener(ros::Duration(2.0)));
-    planning_scene_monitor_ =  boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description"/*, tf_listener_*/);
-    planning_scene_monitor_->startSceneMonitor("planning_scene");
+    planning_scene_monitor_ =  boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description", tf_listener_);
 
-};
+    //Initialize timer & subscriber
+    tf_subscriber = subscribe("joint_states", 1, &ObstacleDistance::joint_state_callback, this);
+    distance_timer = createTimer(ros::Duration(0.5), &ObstacleDistance::getDistanceToObstacles, this);
+
+    //Initialize thread for updating the planning scene
+    boost::thread* ps_thread = new boost::thread(&ObstacleDistance::getPlanningScene, this);
+}
 
 ObstacleDistance::~ObstacleDistance()
 {}
@@ -109,19 +122,9 @@ ObstacleDistance::~ObstacleDistance()
 int main(int argc, char **argv)
 {
     ros::init (argc, argv, "obstacle_distance");
-    ros::AsyncSpinner spinner(2);
-    spinner.start();
-    ObstacleDistance *ob = new ObstacleDistance();
 
-    std::string link_names[] = {"gripper_right_grasp_link", "gripper_left_grasp_link"};
-    std::vector<std::string> links (link_names, link_names + sizeof(link_names) / sizeof(std::string));
-
-    while(ros::ok())
-    {
-        ob->getDistanceToObstacles(links);
-        ros::spinOnce();
-        ros::Duration(0.5).sleep();
-    }
+    ObstacleDistance ob;
+    ros::spin();
 
     ros::shutdown();
     return 0;
