0601d12bb0516762fbd9675ef97f563a60e907ca
Florian Köhler <Florian.Koehler@gmx.net>
2015-09-22 11:10:31 +0200

    added feature to measure distance to all objects and from link chain to objects

--- atf_metrics/src/atf_metrics/calculate_distance_to_obstacles.py
+++ atf_metrics/src/atf_metrics/calculate_distance_to_obstacles.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from atf_msgs.msg import ObstacleDistance
+from types import *
 
 
 class CalculateDistanceToObstaclesParamHandler:
@@ -13,18 +14,22 @@ class CalculateDistanceToObstaclesParamHandler:
         metrics = []
 
         for item in params:
-            metrics.append(CalculateDistanceToObstacles(item[0], item[1]))
+            try:
+                metrics.append(CalculateDistanceToObstacles(item[0], item[1]))
+            except IndexError:
+                metrics.append(CalculateDistanceToObstacles(item[0]))
 
         return metrics
 
 
 class CalculateDistanceToObstacles:
-    def __init__(self, links, collision_object):
+    def __init__(self, links, collision_object="all"):
         self.active = False
         self.finished = False
         self.links = links
         self.collision_objects = collision_object
         self.distances = {}
+        self.start_chain = False
 
         rospy.Subscriber("/atf/obstacle_distance", ObstacleDistance, self.get_distance)
 
@@ -38,13 +43,36 @@ class CalculateDistanceToObstacles:
     def get_distance(self, data):
         if self.active:
             for link in data.links:
-                if link.name == self.links:
-                    for idx, co in enumerate(link.objects):
-                        if co in self.collision_objects:
-                            if link.name + " to " + co not in self.distances:
-                                self.distances[link.name + " to " + co] = []
+                # Single link
+                if type(self.links) is not ListType:
+                    if link.name == self.links:
+                        for idx, co in enumerate(link.objects):
+                            if not self.collision_objects == "all":
+                                if co in self.collision_objects:
+                                    self.save_result(self.distances, link.name, co, link.distances[idx])
+                            else:
+                                self.save_result(self.distances, link.name, co, link.distances[idx])
+                        break
+                else:
+                    # Link chain
+                    if link.name == self.links[0]:
+                        self.start_chain = True
+                    if self.start_chain:
+                        for idx, co in enumerate(link.objects):
+                            if not self.collision_objects == "all":
+                                if co in self.collision_objects:
+                                    self.save_result(self.distances, link.name, co, link.distances[idx])
+                            else:
+                                self.save_result(self.distances, link.name, co, link.distances[idx])
+                    if link.name == self.links[1]:
+                        self.start_chain = False
+                        break
 
-                            self.distances[link.name + " to " + co].append(link.distances[idx])
+    @staticmethod
+    def save_result(save, link_name, co, distance):
+        if link_name + " to " + co not in save:
+            save[link_name + " to " + co] = []
+        save[link_name + " to " + co].append(distance)
 
     def get_result(self):
         if self.finished:
