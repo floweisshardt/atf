66216716f6261dd08bc079662e104192fa403831
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2016-05-13 10:34:55 +0200

    add scripts for recording and analysing all

---
+++ atf_core/scripts/record_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+import rospkg
+import sys
+import os
+
+
+if __name__ == '__main__':
+    r = rospkg.RosPack()
+    pkg = sys.argv[1]
+    print "recording all in package '" + pkg + "'"
+    pkg_path = r.get_path(pkg)
+    path_to_test_files = os.path.join(pkg_path, "test_generated/recording")
+    
+    filenames = []
+    for (dirpath, dirnames, list_of_files) in os.walk(path_to_test_files):
+        for f in list_of_files:
+            if f.endswith(".test"):
+                filenames.append(f)
+
+    print "found " + str(len(filenames)) + " files: " + str(filenames)
+    
+    # record all
+    for f in filenames:
+        command = "rostest " + pkg + " " + f
+        os.system(command)    
