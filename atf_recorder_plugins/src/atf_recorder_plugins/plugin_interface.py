40d923271ec858a2e04b7938b2645f249f72f841
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2016-05-26 01:37:10 +0200

    fix timestamps

--- atf_recorder_plugins/src/atf_recorder_plugins/plugin_interface.py
+++ atf_recorder_plugins/src/atf_recorder_plugins/plugin_interface.py
@@ -3,6 +3,7 @@ import rospy
 import rosgraph
 import rosservice
 import socket
+import time
 from atf_recorder import BagfileWriter
 from rosapi.srv import Nodes, Topics, Publishers, Subscribers
 from atf_msgs.msg import Api, NodeApi, InterfaceItem
@@ -47,7 +48,7 @@ class RecordInterface:
         #print "api=\n", api
 
         # write api to bagfile
-        self.BfW.write_to_bagfile("/atf/" + msg.name + "/api", api, rospy.Time.now())
+        self.BfW.write_to_bagfile("/atf/" + msg.name + "/api", api, rospy.Time.from_sec(time.time()))
 
     def get_service_types(self, services):
         service_types = []
