c10547d40802684a70712e5a2a7b93d691d693e5
fmw-hb <hannes.bachter@ipa.fraunhofer.de>
2017-06-21 15:43:41 +0200

    changed robots maker

--- hannes_test/scripts/robots_maker.py
+++ hannes_test/scripts/robots_maker.py
@@ -8,12 +8,11 @@ import os.path
 
 class robot_maker():
     def __init__(self):
-        self.int_res = [0.35] #[0.25, 0.35, 0.5, 0.6, 0.75, 1.0, 1.5]
-        self.part_num = [100] #[25, 50, 75, 100, 150, 200, 500]
-        self.cb_counter = [1, 5, 10]
+        self.int_res = [0.35. 0.40, 0.45, 0.5] #[0.25, 0.35, 0.5, 0.6, 0.75, 1.0, 1.5]
+        self.part_num = [100, 200, 300, 500] #[25, 50, 75, 100, 150, 200, 500]
         self.mapping = [1]
         self.ndt_match = ["true"]
-        self.gain = [5, 25, 50]
+        self.gain = [2, 3, 5, 8]
         self.dyn_gain = [0.5]
         self.file_path = "/home/fmw-hb/Desktop/robots/"  #'/home/fmw-hb/atf_catkin_ws/src/atf/hannes_test/config/robots/'
         self.robots = ""
@@ -24,34 +23,33 @@ class robot_maker():
             for res in self.int_res:
                 for ndt in self.ndt_match:
                     for gain in self.gain:
-                        for counter in self.cb_counter:
-                            for dyn_gain in self.dyn_gain:
-                                for mapping in self.mapping:
+                        for dyn_gain in self.dyn_gain:
+                            #for mapping in self.mapping:
 
-                                    if(ndt == "true"):
-                                        filename = str(mapping)+"mapping_"+str(dyn_gain)+"overlaymap"
-                                    else:
-                                        filename = str(gain)+"gain_"+"part"+str(part)+"_"+str(int(res*1000))+"mm"
-                                    # save everything in the file
-                                    #with open(self.file_path+filename, 'w') as stream:
-                                    #print "Path: "+self.file_path+filename
-                                    print filename
-                                    robot = open(self.file_path+filename+".yaml", 'w')
-                                    robot.write("path_length:\n  topics:\n    - \"/tf\"\n    - \"/scan_unified\"\nrobot_bringup_launch: \"launch/all.launch\"\nwait_for_topics: []\nwait_for_services: []\n"
-                                                 "additional_parameters:\n  \"/use_sim_time\": true\n"
-                                                 "additional_arguments:\n  \"int_res\": "+str(res)+"\n  \"part_num\": "+str(part)+"\n  \"ndt_match\": "+ndt+"\n  \"gain\": "+str(gain)+
-                                                "\n  \"counter\": "+str(counter)+"\n  \"mapping\": "+str(mapping)+"\n  \"stmgain\": "+str(dyn_gain))
-                                        # stream.write(yaml.dump({'path_length': {'topics': [{'"/tf"', '"/scan_unified"'}]
-                                        #                         'robot_bringup_launch': '"launch/all.launch"'
-                                        #                         'wait_for_topics': '[]'
-                                        #                         'wait_for_services': '[]'
-                                        #                         'additional_parameters':
-                                        #                           '"/use_sim_time"': true
-                                        #                         additional_arguments:
-                                        #                           "int_res": 0.35
-                                        #                           "part_num": 75
-                                        #                           "ndt_match": true}, default_flow_style=False))
-                                    self.robots = self.robots + "    - " + filename + "\n"
+                            if(ndt == "true"):
+                                filename = str(int(int_res*100))+"mm_"+str(part)+"part"+str(gain)+
+                            else:
+                                filename = str(gain)+"gain_"+"part"+str(part)+"_"+str(int(res*1000))+"mm"
+                            # save everything in the file
+                            #with open(self.file_path+filename, 'w') as stream:
+                            #print "Path: "+self.file_path+filename
+                            print filename
+                            robot = open(self.file_path+filename+".yaml", 'w')
+                            robot.write("path_length:\n  topics:\n    - \"/tf\"\n    - \"/scan_unified\"\nrobot_bringup_launch: \"launch/all.launch\"\nwait_for_topics: []\nwait_for_services: []\n"
+                                         "additional_parameters:\n  \"/use_sim_time\": true\n"
+                                         "additional_arguments:\n  \"int_res\": "+str(res)+"\n  \"part_num\": "+str(part)+"\n  \"ndt_match\": "+ndt+"\n  \"gain\": "+str(gain)+
+                                        "\n  \"counter\": "+str(counter)+"\n  \"mapping\": "+str(mapping)+"\n  \"stmgain\": "+str(dyn_gain))
+                                # stream.write(yaml.dump({'path_length': {'topics': [{'"/tf"', '"/scan_unified"'}]
+                                #                         'robot_bringup_launch': '"launch/all.launch"'
+                                #                         'wait_for_topics': '[]'
+                                #                         'wait_for_services': '[]'
+                                #                         'additional_parameters':
+                                #                           '"/use_sim_time"': true
+                                #                         additional_arguments:
+                                #                           "int_res": 0.35
+                                #                           "part_num": 75
+                                #                           "ndt_match": true}, default_flow_style=False))
+                            self.robots = self.robots + "    - " + filename + "\n"
 
         meta = open(self.file_path+"robots", 'wa')
         meta.write(self.robots)
