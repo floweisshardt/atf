ebbec25cc322633fba35fbf16e7362192e8708e2
floweisshardt <florian.weisshardt@mojin-robotics.de>
2019-08-26 20:29:54 +0200

    add user_result metric

--- atf_core/src/atf_core/atf.py
+++ atf_core/src/atf_core/atf.py
@@ -2,7 +2,7 @@
 import rospy
 import atf_core
 
-from atf_msgs.msg import TestblockTrigger
+from atf_msgs.msg import MetricResult, TestblockResult, TestblockTrigger
 from smach_msgs.msg import SmachContainerStatus
 
 ###########
@@ -25,41 +25,39 @@ class ATF():
         #print "current test:", test.name
         self.test = test
 
-        self.publisher = {}
-        #for testblock in self.test.test_config.keys():
-        #    print "testblock", testblock
-        self.publisher = rospy.Publisher("atf/trigger", TestblockTrigger, queue_size=10)
-        rospy.Subscriber("/state_machine/machine/smach/container_status", SmachContainerStatus, self.sm_status_cb)
+        self.publisher_trigger = rospy.Publisher("atf/trigger", TestblockTrigger, queue_size=10)
+        self.publisher_user_result = rospy.Publisher("atf/user_result", TestblockResult, queue_size=10)
+        rospy.Subscriber("/state_machine/machine/smach/container_status", SmachContainerStatus, self._sm_status_cb)
 
         # make sure to wait with the application for the statemachine in sm_test.py to be initialized
         rospy.loginfo("waiting for smach container in test_sm to be ready...")
         rospy.wait_for_message("/state_machine/machine/smach/container_status", rospy.AnyMsg)
-        rospy.sleep(1)
+        #rospy.sleep(3) # FIXME remove
         rospy.loginfo("...smach container in sm_test is ready.")
     
     def start(self, testblock):
         if testblock not in self.test.test_config.keys():
             error_msg = "testblock %s not in list of testblocks"%testblock
-            self.error(error_msg)
+            self._send_error(error_msg)
             raise atf_core.ATFError(error_msg)
         rospy.loginfo("starting testblock %s"%testblock)
         trigger = TestblockTrigger()
         trigger.stamp = rospy.Time.now()
         trigger.name = testblock
         trigger.trigger = TestblockTrigger.START
-        self.publisher.publish(trigger)
+        self.publisher_trigger.publish(trigger)
 
     def stop(self, testblock):
         if testblock not in self.test.test_config.keys():
             error_msg = "testblock %s not in list of testblocks"%testblock
-            self.error(error_msg)
+            self._send_error(error_msg)
             raise atf_core.ATFError(error_msg)
         rospy.loginfo("stopping testblock %s"%testblock)
         trigger = TestblockTrigger()
         trigger.stamp = rospy.Time.now()
         trigger.name = testblock
         trigger.trigger = TestblockTrigger.STOP
-        self.publisher.publish(trigger)
+        self.publisher_trigger.publish(trigger)
 
     def shutdown(self):
         rospy.logdebug("shutting down atf application")
@@ -78,12 +76,29 @@ class ATF():
         rospy.sleep(3) # FIXME we need to wait until sm_test.py is shutdown properly so that bag file can be closed by recorder.py in on_shutdown
         rospy.logdebug("atf application is shutdown.")
 
-    def error(self, error_msg):
+    def set_user_result(self, testblock, data, details):
+        if not isinstance(data, float) and not isinstance(data, int):
+            error_msg = "user_result.data is not a float or int. data=%s, type=%s"%(str(data), type(data))
+            self._send_error(error_msg)
+            raise atf_core.ATFError(error_msg)
+        if type(details) is not list:
+            error_msg = "user_result.details is not a list. detail=%s"%str(details)
+            self._send_error(error_msg)
+            raise atf_core.ATFError(error_msg)
+        metric_result = MetricResult()
+        metric_result.data = data
+        metric_result.details = details
+        testblock_result = TestblockResult()
+        testblock_result.name = testblock
+        testblock_result.results.append(metric_result)
+        self.publisher_user_result.publish(testblock_result)
+
+    def _send_error(self, error_msg):
         trigger = TestblockTrigger()
         trigger.stamp = rospy.Time.now()
         trigger.name = error_msg
         trigger.trigger = TestblockTrigger.ERROR
-        self.publisher.publish(trigger)
+        self.publisher_trigger.publish(trigger)
 
-    def sm_status_cb(self, msg):
+    def _sm_status_cb(self, msg):
         self.sm_container_status = msg
---
+++ atf_metrics/src/atf_metrics/calculate_user_result.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import rospy
+import math
+
+from atf_core import ATFError
+from atf_msgs.msg import MetricResult, KeyValue
+
+class CalculateUserResultParamHandler:
+    def __init__(self):
+        """
+        Class for returning the corresponding metric class with the given parameter.
+        """
+        pass
+
+    def parse_parameter(self, testblock_name, params):
+        """
+        Method that returns the metric method with the given parameter.
+        :param params: Parameter
+        """
+        metrics = []
+
+        # special case for user_result (can have empty list of parameters, thus adding dummy groundtruth information)
+        if params == []:
+            params.append({"groundtruth":None, "groundtruth_epsilon":None})
+
+        if type(params) is not list:
+            rospy.logerr("metric config not a list")
+            return False
+
+        for metric in params:
+            # check for optional parameters
+            try:
+                groundtruth = metric["groundtruth"]
+                groundtruth_epsilon = metric["groundtruth_epsilon"]
+            except (TypeError, KeyError):
+                #rospy.logwarn_throttle(10, "No groundtruth parameters given, skipping groundtruth evaluation for metric 'user_result' in testblock '%s'"%testblock_name)
+                groundtruth = None
+                groundtruth_epsilon = None
+            metrics.append(CalculateUserResult(groundtruth, groundtruth_epsilon))
+        return metrics
+
+class CalculateUserResult:
+    def __init__(self, groundtruth, groundtruth_epsilon):
+        """
+        Class for collecting the the user result.
+        """
+        self.started = False
+        self.finished = False
+        self.active = False
+        self.groundtruth = groundtruth
+        self.groundtruth_epsilon = groundtruth_epsilon
+        self.testblock_result = None
+
+    def start(self, timestamp):
+        if self.testblock_result != None:
+            raise ATFError("user_result should be None but is already set")
+        self.active = True
+        self.started = True
+
+    def stop(self, timestamp):
+        if self.testblock_result == None:
+            raise ATFError("user_result is not set")
+        self.active = False
+        self.finished = True
+
+    def pause(self, timestamp):
+        # TODO: Implement pause
+        pass
+
+    def purge(self, timestamp):
+        # TODO: Implement purge as soon as pause is implemented
+        pass
+
+    def update(self, topic, msg, t):
+        if topic == "/atf/user_result":
+            self.testblock_result = msg
+
+    def get_topics(self):
+        return ["/atf/user_result"]
+
+    def get_result(self):
+        if len(self.testblock_result.results) != 1:
+            raise ATFError("multiple user results found")
+
+        metric_result = MetricResult()
+        metric_result.name = "user_result"
+        metric_result.started = self.started # FIXME remove
+        metric_result.finished = self.finished # FIXME remove
+        metric_result.data = None
+        metric_result.groundtruth = self.groundtruth
+        metric_result.groundtruth_epsilon = self.groundtruth_epsilon
+        
+        # assign default value
+        metric_result.groundtruth_result = None
+        metric_result.groundtruth_error_message = None
+
+        if metric_result.started and metric_result.finished: #  we check if the testblock was ever started and stopped
+            # calculate metric data
+            metric_result.data = self.testblock_result.results[0].data
+
+            # fill details as KeyValue messages
+            details = []
+            metric_result.details = details
+
+            # evaluate metric data
+            if metric_result.groundtruth != None and metric_result.groundtruth_epsilon != None:
+                if math.fabs(metric_result.groundtruth - metric_result.data) <= metric_result.groundtruth_epsilon:
+                    metric_result.groundtruth_result = True
+                    metric_result.groundtruth_error_message = "all OK"
+                else:
+                    metric_result.groundtruth_result = False
+                    metric_result.groundtruth_error_message = "groundtruth missmatch: %f not within %f+-%f"%(metric_result.data, metric_result.groundtruth, metric_result.groundtruth_epsilon)
+                    #print metric_result.groundtruth_error_message
+
+        if metric_result.data == None:
+            metric_result.groundtruth_result = False
+            metric_result.groundtruth_error_message = "no result"
+
+        #print "\nmetric_result:\n", metric_result
+        return metric_result
