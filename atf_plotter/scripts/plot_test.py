ce24c8a09559cd53b65e3b704c2519946e23fff3
floweisshardt <florian.weisshardt@mojin-robotics.de>
2020-07-09 16:18:50 +0200

    configurable plotter (tests, testblocks, metrics)

--- atf_plotter/scripts/plot_test.py
+++ atf_plotter/scripts/plot_test.py
@@ -47,69 +47,100 @@ class DemoPlotter(object):
         for topic, msg, t in bag.read_messages():
             #print type(msg), type(AtfResult()), isinstance(msg, AtfResult) # check if message is an AtfResult message
             #assert isinstance(msg, AtfResult) # check if message is an AtfResult message
-            self.atf_result = msg
+            if topic == "atf_result":
+                self.atf_result = msg
+            else:
+                print "ERROR: invalid topic name in bag. ATF only expects topic 'atf_result'"
             bag.close()
             break
 
-    def plot_fmw(self):
-        #print "self.atf_result=", self.atf_result.groundtruth_error_message
-        #print "type(atf_result)=", type(self.atf_result)
-
-
+    def get_sorted_plot_dicts(self, atf_result, filter_tests, filter_testblocks, filter_metrics):
         tbm = {}
         tmb = {}
-        for test in self.atf_result.results:
+        bmt = {}
+
+        for test in atf_result.results:
             #print test.name
-            tbm[test.name] = {}
-            tmb[test.name] = {}
+            if len(filter_tests) != 0 and test.name not in filter_tests:
+                continue
             for testblock in test.results:
                 #print "  -", testblock.name
-                tbm[test.name][testblock.name] = {}
+                if len(filter_testblocks) != 0 and testblock.name not in filter_testblocks:
+                    continue
+
                 for metric in testblock.results:
                     #print "    -", metric.name
-                    if metric.name not in tmb[test.name].keys():
-                        tmb[test.name][metric.name] = {}
+                    if len(filter_metrics) != 0 and metric.name not in filter_metrics:
+                        continue
+
+                    # tbm
+                    if test.name                 not in tbm.keys():
+                        tbm[test.name] = {}
+                    if testblock.name            not in tbm[test.name].keys():
+                        tbm[test.name][testblock.name] = {}
                     tbm[test.name][testblock.name][metric.name] = metric
+
+                    # tmb
+                    if test.name                 not in tmb.keys():
+                        tmb[test.name] = {}
+                    if metric.name               not in tmb[test.name].keys():
+                        tmb[test.name][metric.name] = {}
                     tmb[test.name][metric.name][testblock.name] = metric
 
-        print ""
-        print "###########"
-        print "### tbm ###"
-        print "###########"
-        print tbm
+                    # bmt
+                    if testblock.name            not in bmt.keys():
+                        bmt[testblock.name] = {}
+                    if metric.name               not in bmt[testblock.name].keys():
+                        bmt[testblock.name][metric.name] = {}
+                    bmt[testblock.name][metric.name][test.name] = metric
 
-        print ""
-        print "###########"
-        print "### tmb ###"
-        print "###########"
-        print tmb
+        ret = {}
+        ret['tbm'] = tbm
+        ret['tmb'] = tmb
+        ret['bmt'] = bmt
+        return ret
 
-        # plot tmb
+    def merge(self):
+        pass
+
+    def print_structure(self):
+        for test in self.atf_result.results:
+            print test.name
+            for testblock in test.results:
+                print "  -", testblock.name
+                for metric in testblock.results:
+                    print "    -", metric.name
+
+    def plot_fmw(self, style, filter_tests, filter_testblocks, filter_metrics):
+
+        sorted_atf_results = self.get_sorted_plot_dicts(self.atf_result, filter_tests, filter_testblocks, filter_metrics)
+
+        if style not in sorted_atf_results.keys():
+            print "ERROR: style '%s' not implemented"%style
+        plot_dict = sorted_atf_results[style]
+
+        nr_rows = len(plot_dict.keys())
+        nr_cols = 0
+        for col_name, col_data in plot_dict.items():
+            nr_cols = max(len(col_data), nr_cols)
         
-        nr_tests = len(tmb.keys())
-        nr_metrics_max = 0
-        nr_testblocks_max = 0
-        for metric_name, metric_data in tmb.items():
-            nr_metrics = len(metric_data)
-            nr_metrics_max = max(nr_metrics, nr_metrics_max)
-            for testblock_name, testblock_data in metric_data.items():
-                    nr_testblocks = len(testblock_data)
-                    nr_testblocks_max = max(nr_testblocks, nr_testblocks_max)
-        
-        print "\nplot tmb (rows: %d, cols: %d)"%(nr_tests, nr_metrics_max)
+        print "\nplotting in style '%s' (rows: %d, cols: %d)"%(style, nr_rows, nr_cols)
         meanlineprops = dict(linestyle='--', color='purple')
-        fig, axs = plt.subplots(nr_tests, nr_metrics_max, sharex=True, figsize=(20, 15)) # FIXME calculate width with nr_testblocks
+        fig, axs = plt.subplots(nr_rows, nr_cols, sharex=True, figsize=(20, 15)) # FIXME calculate width with nr_testblocks
 
-        for col, test_name in enumerate(tmb):
-            axs[col][0].set_ylabel(test_name, rotation=45)
-            for row, metric_name in enumerate(tmb[test_name]):
+        # always make this a numpy 2D matrix to access rows and cols correclty if nr_row=1 or nr_col=1
+        axs = np.atleast_2d(axs) 
+        axs = axs.reshape(nr_rows, nr_cols) 
+
+        for col, col_name in enumerate(plot_dict):
+            axs[col][0].set_ylabel(col_name, rotation=45)
+            for row, row_name in enumerate(plot_dict[col_name]):
                 labels = []
                 data = []
-                for testblock_name in tmb[test_name][metric_name]:
-                    #print testblock_name
-                    labels.append(testblock_name)
-                    data.append([tmb[test_name][metric_name][testblock_name].data.data])
-                title = metric_name
+                for boxplot_name in plot_dict[col_name][row_name]:
+                    labels.append(boxplot_name)
+                    data.append([plot_dict[col_name][row_name][boxplot_name].data.data])
+                title = row_name
                 axs[0][row].set_title(title)
                 axs[col][row].boxplot(data, showmeans=True, meanline=True, labels=labels, meanprops=meanlineprops, patch_artist=True)
                 axs[col][row].grid(True)
@@ -127,10 +158,11 @@ if __name__ == '__main__':
     # to get info about the file, this could be helpful:
     #   rosrun atf_plotter plot.py info-structure ~/atf_result.txt
 
-    add_metric = lambda sp: sp.add_argument('--metric', '-m', type=str, dest='metric', required=True, help='TBD')
-    add_testblock = lambda sp: sp.add_argument('--testblock', '-tb', type=str, dest='testblock', required=True, help='TBD')
-    add_test = lambda sp: sp.add_argument('--test', '-t', type=str, dest='test', required=True, help='TBD')
+    add_test = lambda sp: sp.add_argument('--test', '-t', type=str, dest='test', default="", help='TBD')
     add_test_case_ident = lambda sp: sp.add_argument('--testident', '-ti', type=str, dest='test_case_ident', required=True, help='like test name without repetition, e.g. ts0_c0_r0_e0_s0')
+    add_testblock = lambda sp: sp.add_argument('--testblock', '-tb', type=str, dest='testblock', default="", help='TBD')
+    add_metric = lambda sp: sp.add_argument('--metric', '-m', type=str, dest='metric', default="", help='TBD')
+    add_style = lambda sp: sp.add_argument('--style', '-s', type=str, dest='style', default='tbm', help='style, e.g. tbm (default) tmb, bmt, ...')
 
 
     parser = argparse.ArgumentParser(
@@ -167,6 +199,10 @@ if __name__ == '__main__':
     add_test_case_ident(sub_parser)
 
     sub_parser = subparsers.add_parser('plot-fmw', help='fmw test plot')
+    add_style(sub_parser)
+    add_test(sub_parser)
+    add_testblock(sub_parser)
+    add_metric(sub_parser)
 
     sub_parser = subparsers.add_parser('compare-a', help='visualize comparison for a given metric in various testblocks of a given test, e.g. path_length in testblock testblock_small and testblock testblock_large from atf_test/ts0_c0_r0_e0_s0_0')
 
@@ -248,7 +284,7 @@ if __name__ == '__main__':
     sys.stdout.flush()
     #dp._quicktest()
 
-    #dp.print_structure()
+    dp.print_structure()
 
 
     if argparse_result.command == 'plot-metric':
@@ -267,7 +303,7 @@ if __name__ == '__main__':
     elif argparse_result.command == 'plot-d':
         dp.plot_aggregated_data_for_all_test_repetitions_for_given_test_ident(argparse_result.test_case_ident)
     elif argparse_result.command == 'plot-fmw':
-        dp.plot_fmw()
+        dp.plot_fmw(argparse_result.style, argparse_result.test, argparse_result.testblock, argparse_result.metric)
     elif argparse_result.command == 'info-structure':
         dp.print_structure()
     else:
