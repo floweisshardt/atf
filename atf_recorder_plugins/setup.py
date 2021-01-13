8839434208b799b19de717e2ed747687fa54085c
fmessmer <felixmessmer@gmail.com>
2021-01-13 15:03:03 +0100

    roscompile noetic_migration

--- atf_recorder_plugins/setup.py
+++ atf_recorder_plugins/setup.py
@@ -1,12 +1,11 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+#!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['atf_recorder_plugins'],
-    package_dir={'': 'src'},
+    package_dir={'': 'src'}
 )
 
 setup(**setup_args)
